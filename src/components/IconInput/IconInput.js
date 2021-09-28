135ff4ce5d1c1592856ec33264588db9796917c7
Remi Marchand <remievan@hotmail.com>
2021-09-27 22:03:51 -0300

    Buildd all components

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderWidth: 1,
+    height: 24
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderWidth: 2,
+    height: 36,
+  }
+}
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  color: inherit;
+`;
+
+const StyledInput = styled.input`
+  height: var(--height);
+  border: none;
+  border-bottom: var(--border-width) solid #000;
+  padding-left: var(--height);
+  font-size: var(--font-size);
+  outline-offset: 2px;
+  
+  font-weight: 700;
+  color: inherit;
+  
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+
+const Wrapper = styled.label`
+  position: relative;
+  width: var(--width);
+  
+  color: ${COLORS.gray700};
+  
+  :hover {
+    color: ${COLORS.black};
+  }
+`;
 
 const IconInput = ({
   label,
@@ -13,7 +64,17 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const { fontSize, iconSize, borderWidth, height } = STYLES[size];
+  return <Wrapper style={{ '--width': `${width}px` }}>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <StyledIcon id={icon} size={iconSize}  />
+    <StyledInput placeholder={placeholder}
+                 style={{ '--font-size': `${fontSize}px`,
+                   '--border-width': `${borderWidth}px`,
+                   '--height': `${height}px`
+                 }}
+    />
+  </Wrapper>
 };
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,34 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.div`
+  width: 270px;
+  padding: ${({size}) => size === 'large' ? '4px' : 'none'};
+  height: ${({size}) => {
+    if (size === 'large') return '24px';
+    if (size === 'medium') return '12px';
+    return '8px';
+  }};
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--border-radius);
+  overflow: hidden;
+`;
+
+const Element = styled.div`
+  width: ${({width}) => `${width}%`};
+  height: 100%;
+  background-color: ${COLORS.primary};
+  border-radius: var(--border-radius) 0 0 var(--border-radius);
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const roundingFactor = size === 'small' ? 8 : 4;
+  const style = {'--border-radius': `${roundingFactor}px`};
+
+  return <Wrapper style={style} size={size}>
+    <Element style={style} role="progressbar" width={value} aria-valuenow={value} aria-valuemin="0" aria-valuemax="100"/>
+  </Wrapper>
 };
 
 export default ProgressBar;
